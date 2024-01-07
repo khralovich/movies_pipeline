cc398875925e4f7ce537392eebece853fa5a37aa
khralovich <khralovich@protonmail.com>
2024-01-07 19:07:18 +0100

    add tmdb calls

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ load_dotenv(dotenv_path)
 
 TMDB_READ_ACCESS_TOKEN = os.getenv("TMDB_READ_ACCESS_TOKEN")
 
-
-url = "https://api.themoviedb.org/3/authentication"
+# https://developer.themoviedb.org/reference/intro/getting-started
+url = "https://api.themoviedb.org/3/movie/popular"
+img_url = "https://image.tmdb.org/t/p/w1280"
 
 headers = {
     "accept": "application/json",
     "Authorization": f"Bearer {TMDB_READ_ACCESS_TOKEN}"
 }
 
-response = requests.get(url, headers=headers)
-
-print(response.text)
+try:
+    response = requests.get(url, headers=headers)
+    print(response.text)
+except NameError:
+    print(NameError)
+finally:
+    print("The 'try except' is finished")
